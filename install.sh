b55d27920ae906eea90adce70eb62e3bd97c1ab5
theaeonsolution <31832290+theaeonsolution@users.noreply.github.com>
2020-07-15 21:09:19 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,3 +9,15 @@ ln -fs $PWD/dotfiles/.gitignore_global ~/.gitignore_global
 
 # Run the new .bash_profile.
 source ~/.bash_profile
+
+# Setup Git
+git config --global user.name "Name"
+git config --global user.email "Email"
+git config --global push.default simple
+git config --global pull.ff only
+git config --global core.autocrlf input
+git config --global core.editor vim
+git config --global diff.tool vimdiff
+git config --global mergetool.prompt true
+git config --global merge.tool vimdiff
+git config --global merge.conflictstyle diff3
